23c8a1706870ef2d8a06168b6a1f21e268d65cdc
Fedor Ivanenko <fedor@fedor.studio>
2026-07-23 17:28:30 +0400

    extract theme schema setting pass

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -251,6 +251,11 @@ export {
 } from "./theme-resolution-pass.js";
 export { ThemeResolverIndex } from "./theme-resolver-index.js";
 export {
+	collectThemeSchemaSettings,
+	type ThemeSchemaSettingIds,
+	type ThemeSchemaSettingPassResult,
+} from "./theme-schema-setting-pass.js";
+export {
 	ThemeSemanticStore,
 	ThemeSemanticTransaction,
 } from "./theme-semantic-store.js";
